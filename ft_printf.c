709dc3d12617050567c279d8ee522d0a2692ee63
m4st4k <desulmehgh@gmail.com>
2025-03-13 14:47:40 +0100

    add

--- ft_printf.c
+++ ft_printf.c
@@ -6,12 +6,13 @@
 /*   By: dbriant <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/24 17:06:24 by dbriant           #+#    #+#             */
-/*   Updated: 2025/03/11 03:32:01 by dbriant          ###   ########.fr       */
+/*   Updated: 2025/03/13 14:46:24 by dbriant          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft/libft.h"
 #include "ft_printf.h"
 #include <stdarg.h>
+#include <stdlib.h>
 
 static	int	ft_printchar(size_t *count, char c)
 {
@@ -31,17 +32,34 @@ static	int	ft_printstr(char *str)
 	return (ft_strlen(str));
 }
 
+static	int	ft_printint(int val)
+{
+	char	*t;
+	size_t	len;
+
+	len = 0;
+	t = ft_itoa(val);
+	len += ft_printstr(t);
+	free(t);
+	t = NULL;
+	return (len);
+}
+
 static	int	ft_firstargcheck(const char *str, va_list list, size_t *count)
 {
-	if (ft_strncmp(&(str[0]), "%c", 2) == 0)
+	if (ft_strncmp(str, "%c", 2) == 0)
 		ft_printchar(count, va_arg(list, int));
-	if (ft_strncmp(&(str[0]), "%s", 2) == 0)
+	if (ft_strncmp(str, "%s", 2) == 0)
 		*count += ft_printstr((char *)va_arg(list, char *));
-	if (ft_strncmp(&(str[0]), "%p", 2) == 0)
+	if (ft_strncmp(str, "%p", 2) == 0)
 		*count += ft_printptr(va_arg(list, void *));
-	if (ft_strncmp(&(str[0]), "%x", 2) == 0)
+	if (ft_strncmp(str, "%d", 2) == 0)
+		*count += ft_printint(va_arg(list, int));
+	if (ft_strncmp(str, "%i", 2) == 0)
+		*count += ft_printint(va_arg(list, int));
+	if (ft_strncmp(str, "%x", 2) == 0)
 		*count += ft_inttohexstr(va_arg(list, unsigned int), 0);
-	if (ft_strncmp(&(str[0]), "%X", 2) == 0)
+	if (ft_strncmp(str, "%X", 2) == 0)
 		*count += ft_inttohexstr(va_arg(list, unsigned int), 1);
 	return (2);
 }
@@ -70,8 +88,8 @@ int	ft_printf(const char *str, ...)
 /*
 int	main(void)
 {
-	printf("%X\n", -1);
-	ft_printf("%X\n", -1);
+	printf("%i\n", 010);
+	ft_printf("%i\n", 010);
 	return (0);
 }
 */
